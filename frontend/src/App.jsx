aa199ee1a13c7a97a9020ccde2cfc185a65088ff
MarcinSiebor <marcinsiebor10@gmail.com>
2025-01-19 16:57:33 +0100

    Features Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
+import Features from './components/Features';
 
 const App = () => {
   return (
     <div>
       <Header />
       <HeroSection />
+      <Features />
     </div>
   );
 };
