9cf29d2b44d7ff194bfd497dfea93de5deefe808
MarcinSiebor <marcinsiebor10@gmail.com>
2025-01-19 17:00:30 +0100

    Footer Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import Features from './components/Features';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Header />
       <HeroSection />
       <Features />
+      <Footer />
     </div>
   );
 };
