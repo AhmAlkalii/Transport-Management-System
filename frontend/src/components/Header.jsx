ce730c0470eca2c609df4299336f9e5b0ad8a1a5
MarcinSiebor <marcinsiebor10@gmail.com>
2025-01-19 16:37:04 +0100

    Header Completed

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="header">
+    <h1>TransportApp</h1>
+    <nav>
+      <a href="#features" className="nav-link">Features</a>
+      <a href="#" className="nav-link">Sign In</a>
+    </nav>
+  </header>
+);
+
+export default Header;
